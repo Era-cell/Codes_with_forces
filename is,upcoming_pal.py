4f9a367c2ba7d99e956c08aa1bffe804d60718c2
Suprith KG <suprith7kg@gmail.com>
2024-05-03 17:01:45 +0530

    Update is,upcoming_pal.py

--- is,upcoming_pal.py
+++ is,upcoming_pal.py
@@ -1,13 +1,6 @@
 def upcoming_pal(s):
     """ considering s isn't a pal """
-    i = 0
-    while s[i] == s[len(s) - i - 1]:
-        i += 1
-    if int(s[i]) < int(s[len(s) - i - 1]):
-        return s[:i] + str(int(s[i]) + 1) + ((len(s) - i - 2) - i) * "0" + str(int(s[i]) + 1) + s[len(s) - i:]
-    else:
-        return s[:i] + str(int(s[i])) + ((len(s) - i - 2) - i) * "0" + str(int(s[i])) + s[len(s) - i:]
-
+    
 def is_pal(s):
     i, j= 0, len(s)-1
     while i<j:
