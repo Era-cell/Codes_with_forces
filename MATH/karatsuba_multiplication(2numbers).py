dd109d668119727909a00cc5f319e467c7434c03
Suprith KG <suprith7kg@gmail.com>
2024-06-05 12:30:05 +0530

    Update karatsuba_multiplication(2numbers).py

--- MATH/karatsuba_multiplication(2numbers).py
+++ MATH/karatsuba_multiplication(2numbers).py
@@ -1,1 +1,14 @@
+"""
+Using divide and conquer
 
+n1 = 123456
+n2 = 567890
+
+n1 = a*10**3 + b
+n2 = c*10**3 + d
+
+n1*n2 = ac*2*(10**(n//2)) + (bc+ad)*10**(n//2) + bd
+
+Also, bc+ad = (a+b)*(c+d) - ac - bd
+
+"""
