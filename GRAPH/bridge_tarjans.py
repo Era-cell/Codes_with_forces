772d7c90545cc2810124075c78a96bdae409ae17
Suprith KG <suprith7kg@gmail.com>
2024-06-10 00:10:17 +0530

    Create bridge_tarjans.py

---
+++ GRAPH/bridge_tarjans.py
@@ -0,0 +1,35 @@
+from collections import defaultdict
+
+n = int(input("Number of edges: "))
+
+adj = defaultdict(list)
+edges = int(input("Enter no of edges: "))
+for _ in range(edges):
+    # input edges from -> to
+    frm, to = list(map(int, input().split(" ")))
+    adj[frm].append(to)
+    adj[to].append(frm)
+
+discovery_time = [-1] * n
+least_discovery_time = [-1] * n
+visited = [0] * n
+time = -1
+
+
+def recur(curr, parent=-1):
+    if visited[curr]: return [discovery_time[curr], []]
+    global time
+    time += 1
+    discovery_time[curr], least_discovery_time[curr] = time, time
+    visited[curr] = 1
+    res = []
+    for node in adj[curr]:
+        if node != parent:
+            least_time, r = recur(node, curr)
+            least_discovery_time[curr] = min(least_discovery_time[curr], least_time)
+            res += r
+            if least_discovery_time[curr] != least_discovery_time[node]: res.append((curr, node))
+    return [least_discovery_time[curr], res]
+
+
+print(recur(0)[1])
