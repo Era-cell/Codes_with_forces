17eb60e954497c3f813a04b693e60941f4aad7b3
Suprith KG <suprith7kg@gmail.com>
2024-09-01 23:19:28 +0530

    Create Hungarian_Algorithm.py

---
+++ OR/Hungarian_Algorithm.py
@@ -0,0 +1,13 @@
+"""
+Assigment of n machines for n jobs, 
+such that minimum/maximum cost
+   Job1 Job2 Job3 Job4 Job5
+A   9    10   12   8    2
+B   4    23   21   3    12
+C   ...
+D   ...
+E   ...
+
+For all the jobs atleast one machine is assigned or vise versa
+"""
+
