741539e8187e95a17eb26eaa2bfadc193847cbd7
Suprith KG <suprith7kg@gmail.com>
2024-09-01 23:37:25 +0530

    Update Hungarian_Algorithm.py

--- OR/Hungarian_Algorithm.py
+++ OR/Hungarian_Algorithm.py
@@ -9,5 +9,19 @@ D   ...
 E   ...
 
 For all the jobs atleast one machine is assigned or vise versa
+
+Step 1: reduce rows
+       - minus all element with smallest
+       - row[i] = row[i]-min(row) for i in len(row)
+Step 2: reduce columns
+Step 3: If row has single 0, mark the column
+         else, skip
+Step 4: Same on column
+Step 5: If no of marked 0's are == n, then that's the answer
+      else, 
+         take the smallest of all uncovered values, say minn
+         subtract minn from unconvered values
+         add minn to all intersections.
+         Unmark all, and repeat from Step 3.
 """
 
